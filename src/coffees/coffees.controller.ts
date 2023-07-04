9cbf9be5993eef643f96041818bc2560422f75ff
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-07-04 12:35:19 +0400

    paginationQuery

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -4,19 +4,36 @@ import {
   Post,
   Body,
   Param,
-  HttpStatus,
-  HttpCode,
+  Patch,
+  Delete,
+  Query,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
-  findAll() {
-    return 'This action returns all coffees';
+  @Get()
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `Tis action returns all coffees.Limit:${limit}, offset:${offset}`;
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action returns ${id} for coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates ${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes ${id} coffee`;
   }
 }
