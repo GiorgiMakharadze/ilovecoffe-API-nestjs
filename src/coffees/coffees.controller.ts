32ce44d6d6fa65a8b40aae81778ef518118497ce
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-07-04 13:43:40 +0400

    validations

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -9,6 +9,8 @@ import {
   Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -26,13 +28,13 @@ export class CoffeesController {
   }
 
   @Post()
-  create(@Body() body) {
-    return this.coffeesServices.create(body);
+  create(@Body() CreateCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesServices.create(CreateCoffeeDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return this.coffeesServices.update(id, body);
+  update(@Param('id') id: string, @Body() UpdateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesServices.update(id, UpdateCoffeeDto);
   }
 
   @Delete(':id')
