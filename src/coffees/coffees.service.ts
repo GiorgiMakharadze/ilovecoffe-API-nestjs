bb3ede9e7a09235ee0caf09b1981453823f578f1
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-07-04 22:44:19 +0400

    realtionships between databases

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -13,11 +13,16 @@ export class CoffeesService {
   ) {}
 
   async findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: ['flavors'],
+    });
   }
 
   async findOne(id: number) {
-    const coffee = await this.coffeeRepository.findOne({ where: { id: id } });
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id: id },
+      relations: ['flavors'],
+    });
     if (!coffee) {
       throw new NotFoundException(`Coffee ${id} not found`);
     }
